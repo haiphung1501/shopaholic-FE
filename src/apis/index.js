bcbc206ce80292de844a5a3afe3884657880abca
haiphung1501 <haiphung15012002@gmail.com>
2023-04-26 10:06:34 +0700

    plsaesae

--- src/apis/index.js
+++ src/apis/index.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
-// const baseURL = "https://shopaholic-api.onrender.com";
-const baseURL = "http://localhost:4000";
+const baseURL = "https://shopaholic-api.onrender.com";
+// const baseURL = "http://localhost:4000";
 
 export const getAllProductReq = (keyword = "") =>
   axios.get(baseURL + `/api/product?search=${keyword}`);
